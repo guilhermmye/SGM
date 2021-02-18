1b1fe4c3a0cb8a8dc07f3be9eb7c324596862940
wilianparreira <willcoro2@hotmail.com>
2021-02-17 22:03:57 -0300

    Modificação

--- mimg-backend/src/main/java/br/gov/prefeitura/mimg/resourse/RegiaoImediataResource.java
+++ mimg-backend/src/main/java/br/gov/prefeitura/mimg/resourse/RegiaoImediataResource.java
@@ -1,40 +1,24 @@
 package br.gov.prefeitura.mimg.resourse;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.prefeitura.mimg.model.RegiaoImediata;
 import br.gov.prefeitura.mimg.repository.RegiaoImediataRepository;
-import br.gov.prefeitura.mimg.repository.regiaoImediata.filter.RegiaoImediataFilter;
-import br.gov.prefeitura.mimg.service.SubdistritoService;
 
 @RestController
 @RequestMapping("/regioesImediatas")
 public class RegiaoImediataResource {
 	
 	@Autowired
-	private RegiaoImediataRepository    regiaoImediataRepository;
-		
-	@Autowired
-	private ApplicationEventPublisher   publisher;
-	
-	@Autowired
-	private SubdistritoService          subdistritoService;
-	
-	
+	private RegiaoImediataRepository  regiaoImediataRepository;
 	
 	@GetMapping
-	public Page<RegiaoImediata> pesquisar(RegiaoImediataFilter regiaoImediataFilter,Pageable pageable){
-		return regiaoImediataRepository.filtrar(regiaoImediataFilter,pageable);
+	public List<RegiaoImediata> listar(){
+		return regiaoImediataRepository.findAll();
 	}
-	
-//	@GetMapping
-//	public List<RegiaoImediata> listar(){
-//		return regiaoImediataRepository.findAll();
-//	}
 }
