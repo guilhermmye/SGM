b8c7da680491347f759179a90c168e988be398c4
guilhermmye <guilhermmye@gmail.com>
2021-03-18 23:40:02 -0300

    ll

--- banco/SEGURANCA/SCRIPT_INSERT_SEGURANCA.sql
+++ banco/SEGURANCA/SCRIPT_INSERT_SEGURANCA.sql
@@ -1,3 +1,9 @@
 INSERT INTO roles(name) VALUES('ROLE_USUARIO');
 INSERT INTO roles(name) VALUES('ROLE_TECNICO');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+
+INSERT INTO public.usuario(email, password, username)
+VALUES ('admin@email.com','$2a$10$Z6IVUsSmd6sOb3JRkgmO3ugcG9f/Q29hN41bzY9MCP.OkwClUMe/6','admin');
+
+INSERT INTO public.usuario_roles(usuario_id, roles_id)
+VALUES (1,3);
